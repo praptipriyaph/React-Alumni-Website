73541407f8cbc46e4f1ccd911cb38170c31dac28
praptipriyaph <phukonpraptipriya@gmail.com>
2024-02-18 23:12:49 +0530

    alumni page commit

--- src/pages/Alumni/Alumni.jsx
+++ src/pages/Alumni/Alumni.jsx
@@ -2,70 +2,80 @@ import React from 'react';
 import './Alumni.css';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-import Alumni1Img from '../../utils/images/blog1-img.jpg';
-import Alumni2Img from '../../utils/images/blog2-img.jpg';
-import Alumni3Img from '../../utils/images/blog3-img.jpg';
-import Alumni4Img from '../../utils/images/blog4-img.jpg';
-import Alumni5Img from '../../utils/images/blog5-img.jpg';
-import Alumni6Img from '../../utils/images/blog6-img.jpg';
-import Alumni7Img from '../../utils/images/blog7-img.jpg';
-import Alumni8Img from '../../utils/images/blog8-img.jpg';
-import Alumni9Img from '../../utils/images/blog9-img.jpg';
+import Alumni1Img from '../../utils/images/Uddipana Dihingia -1 .jpeg';
+import Alumni2Img from '../../utils/images/Shreyasee Dev -2  .jpeg';
+import Alumni3Img from '../../utils/images/Shravan Kumar -3  .jpeg';
+import Alumni4Img from '../../utils/images/Jyotirmoy Poddar -4 .jpeg';
+import Alumni5Img from '../../utils/images/Filza Piya Borah -5  .jpeg';
+import Alumni6Img from '../../utils/images/Debojyoti Paul -6 .jpeg';
+import Alumni7Img from '../../utils/images/Bishal Nath -7 .jpeg';
+import Alumni8Img from '../../utils/images/Arindam Nandi -8.jpeg';
+import Alumni9Img from '../../utils/images/Angshuman Biswas -9 .jpeg';
 
 const alumnis = [
     {
         id: 1,
         img: [Alumni1Img],
         title: 'Uddipana Dihingia',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Pursuing MBA, IIM - Amritsar (2020-21)',
+        designation: 'Batch of 2018, B.Tech CSE'
+        
     },
     {
         id: 2,
         img: [Alumni2Img],
         title: 'Shreyasee Dev',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'PG Diploma in Statistical Methods and Analytics, ISI, Kolkata (2019-20) ',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 3,
         img: [Alumni3Img],
         title: 'Shravan Kumar',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Software Developer, Zwayam Digital Pvt. Ltd., Bangalore ',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 4,
         img: [Alumni4Img],
-        title: 'Jyotirmoy Kumar',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        title: 'Jyotirmoy Poddar',
+        description: 'Software Engineer, Zaloni Technologies, Guwahati',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 5,
         img: [Alumni5Img],
         title: 'Filza Priya Borah',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Associate Software Engineer, International Business Machines Corporation ',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 6,
         img: [Alumni6Img],
         title: 'Debjyoti Paul',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Project Engineer, Wipro Limited',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 7,
         img: [Alumni7Img],
         title: 'Bishal Nath',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Persuing M.E. (Software Systems), BITS - Pilani',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 8,
         img: [Alumni8Img],
         title: 'Arindam Nandi',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Pursuing M.Tech (IT) IIIT - Allahabad',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 9,
         img: [Alumni9Img],
         title: 'Angshuman Biswas',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        description: 'Pursuing M.Tech (CSE) IIT - Guwahati (2020-21)',
+        designation: 'Batch of 2018, B.Tech CSE'
     },
 ];
 
@@ -89,7 +99,7 @@ function Alumni() {
                                     <Card.Img variant="top" src={Alumni.img} />
                                     <Card.Body className='p-md-5 d-flex flex-column align-items-center'>
                                         <Card.Title className='fs-2 mb-4'>{Alumni.title}</Card.Title>
-                                        <Card.Text className='text-center'>{Alumni.description}</Card.Text>
+                                        <Card.Text className='text-center'>{Alumni.description}<br/><br/>{Alumni.designation}</Card.Text>
                                     </Card.Body>
                                 </Card>
                             </Link>
