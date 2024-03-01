6ffac83e992dfa5e7affbc1730a79b29da2ec8f0
Praptipriya Phukon <phukonpraptipriya@gmail.com>
2024-03-01 13:21:45 +0530

    dept photos uploaded

--- src/pages/Alumni/Alumni.jsx
+++ src/pages/Alumni/Alumni.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './Alumni.css';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-import Alumni1Img from '../../utils/images/Uddipana Dihingia -1 .jpeg';
-import Alumni2Img from '../../utils/images/Shreyasee Dev -2  .jpeg';
-import Alumni3Img from '../../utils/images/Shravan Kumar -3  .jpeg';
-import Alumni4Img from '../../utils/images/Jyotirmoy Poddar -4 .jpeg';
-import Alumni5Img from '../../utils/images/Filza Piya Borah -5  .jpeg';
-import Alumni6Img from '../../utils/images/Debojyoti Paul -6 .jpeg';
-import Alumni7Img from '../../utils/images/Bishal Nath -7 .jpeg';
-import Alumni8Img from '../../utils/images/Arindam Nandi -8.jpeg';
-import Alumni9Img from '../../utils/images/Angshuman Biswas -9 .jpeg';
+import Alumni1Img from '../../utils/dept-img/app-sci.jpeg';
+import Alumni2Img from '../../utils/dept-img/civ-engg.jpg';
+import Alumni3Img from '../../utils/dept-img/cse.jpeg';
+import Alumni4Img from '../../utils/dept-img/design.jpeg';
+import Alumni5Img from '../../utils/dept-img/ee.jpeg';
+import Alumni6Img from '../../utils/dept-img/ece.jpg';
+import Alumni7Img from '../../utils/dept-img/energy.png';
+import Alumni8Img from '../../utils/dept-img/fet.jpeg';
+import Alumni9Img from '../../utils/dept-img/mech.jpeg';
 
 const alumnis = [
     {
         id: 1,
         img: [Alumni1Img],
-        title: 'Uddipana Dihingia',
+        title: 'Department of Applied Sciences',
         description: 'Pursuing MBA, IIM - Amritsar (2020-21)',
         designation: 'Batch of 2018, B.Tech CSE'
         
@@ -24,58 +24,149 @@ const alumnis = [
     {
         id: 2,
         img: [Alumni2Img],
-        title: 'Shreyasee Dev',
-        description: 'PG Diploma in Statistical Methods and Analytics, ISI, Kolkata (2019-20) ',
-        designation: 'Batch of 2018, B.Tech CSE'
+        title: 'Department of Civil Engineering',
     },
     {
         id: 3,
         img: [Alumni3Img],
-        title: 'Shravan Kumar',
+        title: 'Department of Computer Science & Engineering',
         description: 'Software Developer, Zwayam Digital Pvt. Ltd., Bangalore ',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 4,
         img: [Alumni4Img],
-        title: 'Jyotirmoy Poddar',
+        title: 'Department of Design',
         description: 'Software Engineer, Zaloni Technologies, Guwahati',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 5,
         img: [Alumni5Img],
-        title: 'Filza Priya Borah',
+        title: 'Department of Electrical Engineering',
         description: 'Associate Software Engineer, International Business Machines Corporation ',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 6,
         img: [Alumni6Img],
-        title: 'Debjyoti Paul',
+        title: 'Department of Electronics & Communication  Engineering',
         description: 'Project Engineer, Wipro Limited',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 7,
         img: [Alumni7Img],
-        title: 'Bishal Nath',
+        title: 'Department of Energy',
         description: 'Persuing M.E. (Software Systems), BITS - Pilani',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 8,
         img: [Alumni8Img],
-        title: 'Arindam Nandi',
+        title: 'Department of Food Engineering & Technology',
         description: 'Pursuing M.Tech (IT) IIIT - Allahabad',
         designation: 'Batch of 2018, B.Tech CSE'
     },
     {
         id: 9,
         img: [Alumni9Img],
-        title: 'Angshuman Biswas',
-        description: 'Pursuing M.Tech (CSE) IIT - Guwahati (2020-21)',
-        designation: 'Batch of 2018, B.Tech CSE'
+        title: 'Department of Mechanical Engineering',
+    },
+    {
+        id: 10,
+        img: [Alumni9Img],
+        title: 'Department of Assamese',
+    },
+    {
+        id: 11,
+        img: [Alumni9Img],
+        title: 'Department of Cultural Studies',
+    },
+    {
+        id: 12,
+        img: [Alumni9Img],
+        title: 'Department of Cultural Studies',
+    },
+    {
+        id: 13,
+        img: [Alumni9Img],
+        title: 'Department of English',
+    },
+    {
+        id: 14,
+        img: [Alumni9Img],
+        title: 'Department of Foreign Languages',
+    },
+    {
+        id: 15,
+        img: [Alumni9Img],
+        title: 'Department of Linguistics & Language Technology',
+    },
+    {
+        id: 16,
+        img: [Alumni9Img],
+        title: 'Department of Mass Communication & Journalism',
+    },
+    {
+        id: 17,
+        img: [Alumni9Img],
+        title: 'Department of Sociology',
+    },
+    {
+        id: 18,
+        img: [Alumni9Img],
+        title: 'Department of Hindi',
+    },
+    {
+        id: 19,
+        img: [Alumni9Img],
+        title: 'Department of Social Work',
+    },
+    {
+        id: 20,
+        img: [Alumni9Img],
+        title: 'Department of Education',
+    },
+    {
+        id: 21,
+        img: [Alumni9Img],
+        title: 'Department of Law',
+    },
+    {
+        id: 22,
+        img: [Alumni9Img],
+        title: 'Department of Business Administration',
+    },
+    {
+        id: 23,
+        img: [Alumni9Img],
+        title: 'Department of Commerce',
+    },
+    {
+        id: 24,
+        img: [Alumni9Img],
+        title: 'Department of Chemical Science',
+    },
+    {
+        id: 25,
+        img: [Alumni9Img],
+        title: 'Department of Environmental Science',
+    },
+    {
+        id: 26,
+        img: [Alumni9Img],
+        title: 'Department of Mathematical Science',
+    },
+    {
+        id: 27,
+        img: [Alumni9Img],
+        title: 'Department of Molecular Biology & Biotechnology',
+    },
+    {
+        id: 28,
+        img: [Alumni9Img],
+        title: 'Department of Physics',
     },
 ];
 
